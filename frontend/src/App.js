856e076da424b4727f5aff32604dd9ec73ed1142
Jane-Njoroge <njorogejwangui@gmail.com>
2025-02-27 10:49:22 +0300

    updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,29 @@
-
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import WelcomeScreen from "./pages/WelcomeScreen";
 import SignUp from "./pages/Signup";
-import Login from "./pages/Login"
+import Login from "./pages/Login";
 import ForgotPassword from "./pages/ForgotPassword";
 import LoadingScreen from "./pages/LoadingScreen";
 import AddPlayerPage from "./pages/AddPlayerPage";
-import NewGamePage from "./pages/NewGamePage"
+import NewGamePage from "./pages/NewGamePage";
 import PlayerInfo from "./components/PlayerInfo";
 import RaceCar from "./components/RaceCar";
 import MonopolyTimer from "./components/MonopolyTimer";
+import Board from "./components/Board";
 
 //import MonopolyPage from "./MonopolyPage"
 
-
 function App() {
   return (
-  
-      <Router>
-        <Routes>
-          <Route path='/' element={<WelcomeScreen/>}/>
-          <Route path='/Signup' element={<SignUp/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path="forgot-password" element={<ForgotPassword/>}/>
-         <Route path='/loading' element={<LoadingScreen/>}/>  
-          <Route path='/add-player' element={<AddPlayerPage/>}/>
-         <Route path='/new-game' element={<NewGamePage/>}/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<WelcomeScreen />} />
+        <Route path="/Signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="forgot-password" element={<ForgotPassword />} />
+        <Route path="/loading" element={<LoadingScreen />} />
+        <Route path="/add-player" element={<AddPlayerPage />} />
+        <Route path="/new-game" element={<NewGamePage />} />
         {/* <Route path="/"element={<PlayerInfo/>}/>
         <Route path="" element={<PlayerInfo />}/>
          <Route path="" element={<RaceCar />}/>
@@ -37,15 +35,12 @@ function App() {
               <PlayerInfo />
               <RaceCar />
               <MonopolyTimer />
+              <Board />
             </>
           }
         />
-        </Routes>
-      </Router>
-
-
-
-  
+      </Routes>
+    </Router>
   );
 }
 
